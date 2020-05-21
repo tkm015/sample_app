f8eae47cee3b2a34e2364f8ff1fe14e49691385a
tkm015 <takumi200015@yahoo.co.jp>
2020-05-21 14:22:29 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e02c96529af9b0fb0d2bf0e0860fb61d70b9ccea2e8d968536bf440f135c0df27256870edc2a85f70f67f18c02526067d8e07c90fee329777a42047db63423e4'
